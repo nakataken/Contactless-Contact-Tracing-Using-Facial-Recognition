ccf3459aff19d58c68cffe5e0ff46b48a7237c40
Ken Nakata <73516285+nakataken@users.noreply.github.com>
2022-03-04 20:03:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const adminRoute = require("./routes/adminRoutes.js");
 
 const app = express();
 
-<<<<<<< HEAD
 app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-=======
-app.use(express.static('public'));
+app.use(express.json({limit: '50mb'}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+app.set('view engine', 'ejs');
 
 // app.use(express.json());
 app.use(express.json({limit: '50mb'}));
